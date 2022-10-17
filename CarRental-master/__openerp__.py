9fa58cb3c31fcd6f14fd33b358797a518f3b667f
fahmialfurqon <101053168+fahmialfurqon@users.noreply.github.com>
2022-10-17 16:36:50 +0700

    Add files via upload

---
+++ CarRental-master/__openerp__.py
@@ -0,0 +1,17 @@
+{
+  
+   'name' : 'Car Rental Module',
+   'author' : 'M Fahmi Al Furqon',
+   'website':'https://github.com/fahmialfurqon/odoo-car-rental.git',
+   'version' : '1.0',
+   'description' : '''This is a module for automating the car rental process''',
+   'depends' : ['base' , 'product'],
+   'category':'General',
+   'init_xml':[],
+   'data':['car_rental_view.xml'],
+   'demo_xml':[],
+   'active':False,
+   'installable':True,
+   'certificate':''
+    
+}
